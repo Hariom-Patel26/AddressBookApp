decb2559a67afd459f4df8953a7556b2b7d0fbe7
Hariompatel26 <hariompatel2611@gmail.com>
2026-03-09 17:04:09 +0530

    [Hariom] :Implemented UC2 - add contact functionality in AddressBook with REST API and tests.

--- src/main/java/com/addressbook/AddressBookAppApplication.java
+++ src/main/java/com/addressbook/AddressBookAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AddressBookAppApplication {
+public class AddressBookAppApplication{
 
-	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookAppApplication.class, args);
+        System.out.println("Welcome to Address Book Application");
+    }
 }
